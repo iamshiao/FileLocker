157a62f53f2d1ba3202858c2d2f0c778dfff76ac
iamshiao <iamshiao@gmail.com>
2021-02-25 14:10:59 +0800

    Init project content

---
+++ FileLocker/Form_locker.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace FileLocker
+{
+    partial class Form_locker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox_path = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button_unlock = new System.Windows.Forms.Button();
+            this.button_lock = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_path
+            // 
+            this.textBox_path.Location = new System.Drawing.Point(12, 12);
+            this.textBox_path.Name = "textBox_path";
+            this.textBox_path.Size = new System.Drawing.Size(613, 22);
+            this.textBox_path.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button_unlock
+            // 
+            this.button_unlock.Location = new System.Drawing.Point(550, 40);
+            this.button_unlock.Name = "button_unlock";
+            this.button_unlock.Size = new System.Drawing.Size(75, 23);
+            this.button_unlock.TabIndex = 2;
+            this.button_unlock.Text = "unlock";
+            this.button_unlock.UseVisualStyleBackColor = true;
+            this.button_unlock.Click += new System.EventHandler(this.button_unlock_Click);
+            // 
+            // button_lock
+            // 
+            this.button_lock.Location = new System.Drawing.Point(12, 40);
+            this.button_lock.Name = "button_lock";
+            this.button_lock.Size = new System.Drawing.Size(75, 23);
+            this.button_lock.TabIndex = 3;
+            this.button_lock.Text = "lock";
+            this.button_lock.UseVisualStyleBackColor = true;
+            this.button_lock.Click += new System.EventHandler(this.button_lock_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 126);
+            this.Controls.Add(this.button_lock);
+            this.Controls.Add(this.button_unlock);
+            this.Controls.Add(this.textBox_path);
+            this.Name = "Form1";
+            this.Text = "FileLocker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_path;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button_unlock;
+        private System.Windows.Forms.Button button_lock;
+    }
+}
+
---
+++ FileLocker/Form_locker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileLocker
+{
+    public partial class Form_locker : Form
+    {
+        FileStream _fs;
+
+        public Form_locker()
+        {
+            InitializeComponent();
+        }
+
+        private void button_lock_Click(object sender, EventArgs e)
+        {
+            _fs = new FileStream(textBox_path.Text, FileMode.Open, FileAccess.Read, FileShare.None);
+        }
+
+        private void button_unlock_Click(object sender, EventArgs e)
+        {
+            _fs.Close();
+            _fs.Dispose();
+        }
+    }
+}
